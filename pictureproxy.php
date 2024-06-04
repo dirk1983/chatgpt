4f22dca8a8236c8c9adeb985d3b841fa023c78d5
dirk1983 <xiaoduzhen@gmail.com>
2024-06-04 13:16:59 +0800

    Update pictureproxy.php

--- pictureproxy.php
+++ pictureproxy.php
@@ -1,8 +1,14 @@
 <?php
 if (isset($_GET['url'])) {
-    $image = file_get_contents($_GET['url']);
-    header("Content-type: image/jpeg");
-    echo $image;
+    $url = $_GET['url'];
+    $headers = get_headers($url, 1);
+    if (isset($headers['Content-Type']) && strpos($headers['Content-Type'], 'image/')!== false) {
+        $image = file_get_contents($url);
+        header("Content-type: image/jpeg");
+        echo $image;
+    } else {
+        echo "Invalid request: not an image file";
+    }
 } else {
     echo "Invalid request";
 }
