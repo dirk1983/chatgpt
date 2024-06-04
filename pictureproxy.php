f7a0637b5f2e0a201b5feba0648328ffca352e4f
dirk1983 <xiaoduzhen@gmail.com>
2024-06-04 13:32:05 +0800

    Update pictureproxy.php

--- pictureproxy.php
+++ pictureproxy.php
@@ -1,9 +1,17 @@
 <?php
-if (isset($_GET['url'])) {
-    $url = $_GET['url'];
-    $headers = get_headers($url, 1);
-    if (isset($headers['Content-Type']) && strpos($headers['Content-Type'], 'image/')!== false) {
-        $image = file_get_contents($url);
+if ((isset($_GET['url'])) && (strpos($_GET['url'], 'http') === 0)) {
+    $headers = get_headers($_GET['url'], 1);
+    if (isset($headers['Content-Type']) && strpos($headers['Content-Type'], 'image/') !== false) {
+        $context = stream_context_create([
+            'http' => [
+                'timeout' => 30,
+            ],
+            "ssl" => [
+                "verify_peer" => false,
+                "verify_peer_name" => false,
+            ],
+        ]);
+        $image = file_get_contents($_GET['url'], false, $context);
         header("Content-type: image/jpeg");
         echo $image;
     } else {
